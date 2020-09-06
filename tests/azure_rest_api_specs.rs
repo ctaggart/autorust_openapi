2be2422317c1c36472c4b069e13caf7662a4844d
Cameron Taggart <cameron.taggart@gmail.com>
2020-09-06 10:05:45 -0600

    integration test azure_rest_api_specs (#9)
    * integration test azure_rest_api_specs
    
    * cd autorust_openapi

---
+++ tests/azure_rest_api_specs.rs
@@ -0,0 +1,12 @@
+mod common;
+use common::*;
+
+// cargo test --test azure_rest_api_specs
+// These tests require cloning azure-rest-api-specs.
+// git clone git@github.com:Azure/azure-rest-api-specs.git ../azure-rest-api-specs
+#[test]
+fn can_deserialize_azure_rest_api_specs() -> Result<()> {
+    assert_deserialize_without_ignored(vec![
+        "../azure-rest-api-specs/specification/vmware/resource-manager/Microsoft.AVS/stable/2020-03-20/vmware.json",
+    ])
+}
