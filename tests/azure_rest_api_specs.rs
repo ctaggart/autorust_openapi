f7f265512a7231a593119bb01ae1f1e48ec8138f
Ryan Levick <ryan.levick@gmail.com>
2020-09-28 14:26:08 +0200

    Add x_ms_error_response

--- tests/azure_rest_api_specs.rs
+++ tests/azure_rest_api_specs.rs
@@ -8,6 +8,7 @@ const PATHS: &[&str] = &[
     "../azure-rest-api-specs/specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2020-04-01/cosmos-db.json",
     "../azure-rest-api-specs/specification/alertsmanagement/resource-manager/Microsoft.AlertsManagement/preview/2019-05-05-preview/ActionRules.json",
     "../azure-rest-api-specs/specification/apimanagement/resource-manager/Microsoft.ApiManagement/stable/2019-12-01/apimapis.json",
+    "../azure-rest-api-specs/specification/communication/data-plane/Microsoft.CommunicationServicesChat/preview/2020-09-21-preview2/communicationserviceschat.json",
 ];
 
 // cargo test --test azure_rest_api_specs
